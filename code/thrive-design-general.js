0a3525d713cb09fcdc0fb92fb403fcd79eef283f
Nicola Hastings <nhastings@higherlogic.com>
2023-04-26 16:17:36 -0400

    NH | CTA tiles

---
+++ code/thrive-design-general.js
@@ -0,0 +1,27 @@
+function handleLink(self) {
+    var link = !!($(self).find('h3 a').attr('href')) ? $(self).find('h3 a') : $(self).find('a'),
+        href = $(link).attr('href'),
+        target = $(link).attr('target');
+
+    if (target == '_blank') {
+        $(self).wrapInner('<a href="' + href + '" target="_blank" rel="noopener" />');
+    } else {
+        $(self).wrapInner('<a href="' + href + '" />');
+    }
+
+    if ($(link).parent().is('h3')) {
+        $(link).contents().unwrap();
+    } else {
+        $(link).hide();
+    }
+}
+
+function handleCTATiles() {
+    $('.cta-tile .HtmlContent').each(function() {
+        handleLink(this);
+    });
+}
+
+$(function () {
+    handleCTATiles();
+});
