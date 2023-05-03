d9d62252b87fa242cb9a5e39057afab59bc87bf4
Nicola Hastings <nhastings@higherlogic.com>
2023-05-03 08:40:42 -0400

    NH | sticker sheet

--- code/thrive-design-sponsors.js
+++ code/thrive-design-sponsors.js
@@ -2,11 +2,26 @@ $(function () {
     $('.sponsors > .col-md-12').slick({
         dots: false,
         arrows: false,
-        autoplay: true,
+        autoplay: false,
         slidesToShow: 5,
         slidesToScroll: 1,
         centerMode: true,
-        centerPadding: 'calc(50% - 600px)'
+        centerPadding: 'calc(50% - 592px)',
+        responsive: [
+            {
+                breakpoint: 1216,
+                settings: {
+                    centerPadding: '8px',
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    centerMode: false
+                }
+            }
+        ]
     });
 
     $('.sponsor .HtmlContent > p').contents().unwrap();
