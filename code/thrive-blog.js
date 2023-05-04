f5f45ce65bf1ea1c8bbe0d428cc3aa5cbd47211c
eCM-MDykeman <matthew@econversemedia.com>
2023-05-04 08:36:50 -0400

    Adding in blog/resource styling

--- code/thrive-blog.js
+++ code/thrive-blog.js
@@ -1,13 +1,26 @@
-$(function(){
+$(function () {
 
-    $('.top-blogs .blogs-block').wrapAll('<div class="top-blogs-slider"></div>');
+    $('.top-blogs ul li').wrapAll('<div class="top-blogs-slider"></div>');
+
+    $('.top-blogs').wrap('<div class="top blogs-slider-wrapper"></div>');
+
+    $('.blogs-slider-wrapper ul li').each(function () {
+        handleAjaxCall(this);
+    });
+
+    $('.thrive-blogs .blogs-block').each(function () {
+        handleAjaxCall(this);
+    });
 
     $('.top-blogs-slider').slick({
         dots: false,
         arrows: true,
         infinite: true,
-        fade: true,
-        autoplay: true
+        fade: false,
+        // autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
     });
+
 
 });
