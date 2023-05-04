41cfef0c1999797be0723aa43849921acf258057
eCM-MDykeman <64661174+eCM-MDykeman@users.noreply.github.com>
2023-05-04 16:42:48 -0400

    Moving thrive-blogs.js and adding ajax function to general

--- code/thrive-design-general.js
+++ code/thrive-design-general.js
@@ -46,6 +46,32 @@ function handleLinkCards() {
     });
 }
 
+function handleAjaxCall(self) {
+    var href = $(self).find('h3 a').attr('href');
+
+    // handle image 
+
+    var imgContainer = '<div class="img-container loading" />';
+    $(self).prepend(imgContainer);
+    $.ajax({
+        url: href,
+        dataType: 'html',
+        success: success
+    });
+    
+    function success(resp) {
+        var img = $(resp).find('.blogs-block > div[id*="UpdatePanel"] > .row:not(.margin-bottom-medium) > .col-md-12 img:first-of-type'),
+            src = $(img).attr('src');
+
+        if (!!src) {
+            var url = "url('" + src + "')";
+            $(self).find('.img-container').css('background-image', url);
+            $(self).find('.img-container').removeClass('loading');
+        }
+            
+    }
+}
+
 $(function () {
     handleInteriorPadding();
     handleLinkCards();
