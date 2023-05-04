41613952f6b9e1df5a313661a1fd53b8efb522be
eCM-MDykeman <64661174+eCM-MDykeman@users.noreply.github.com>
2023-05-04 17:14:57 -0400

    handle library ajax calls

--- code/thrive-design-general.js
+++ code/thrive-design-general.js
@@ -63,17 +63,52 @@ function handleAjaxCall(self) {
         var img = $(resp).find('.blogs-block > div[id*="UpdatePanel"] > .row:not(.margin-bottom-medium) > .col-md-12 img:first-of-type'),
             src = $(img).attr('src');
 
-      if(!!src) {
-        var img = $(resp).find('.col-md-10 .media img'),
-        src = $(img).attr('src');
-      }
-
         if (!!src) {
             var url = "url('" + src + "')";
             $(self).find('.img-container').css('background-image', url);
             $(self).find('.img-container').removeClass('loading');
         }
             
+    }
+}
+
+function handleLibraryAjax(self, asBackground) {
+    var href = $(self).find('h3 a').attr('href');
+
+    // handle image 
+
+    if (!asBackground) {
+        var imgContainer = '<div class="img-container loading" />';
+        $(self).wrapInner('<div class="text-container" />');
+        $(self).prepend(imgContainer);
+    }
+    $.ajax({
+        url: href,
+        dataType: 'html',
+        success: success,
+        error: removeLoading
+    });
+    
+    function success(resp) {
+        var img = $(resp).find('div[id*="DetailPanel"] > .row  .row.margin-bottom-medium > .col-md-12 img:first-of-type'),
+            src = $(img).attr('src');
+
+        if (!!src) {
+            var url = "url('" + src + "')";
+            if (asBackground) {
+                $(self).css('background-image', url);
+            } else {
+                $(self).find('.img-container').css('background-image', url);
+            }
+        } else {
+            $(self).find('.img-container').addClass('no-ajax-image');
+        }
+        
+        removeLoading();
+    }
+
+    function removeLoading() {
+        $(self).find('.img-container').removeClass('loading');
     }
 }
 
