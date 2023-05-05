4836e9cba8550ccce274debf8235ca2da19f15c4
Nicola Hastings <nhastings@higherlogic.com>
2023-05-05 12:21:45 -0400

    NH | updating ajax images JS to preferentially use featured image for blogs

--- code/thrive-design-ajax-call.js
+++ code/thrive-design-ajax-call.js
@@ -1,26 +1,44 @@
-function handleAjaxCall(self) {
+function handleAjaxCall(self, asBackground) {
     var href = $(self).find('h3 a').attr('href');
 
     // handle image 
 
-    var imgContainer = '<div class="img-container loading" />';
-    $(self).prepend(imgContainer);
+    if (!asBackground) {
+        var imgContainer = '<div class="img-container loading" />';
+        $(self).wrapInner('<div class="text-container" />');
+        $(self).prepend(imgContainer);
+    }
     $.ajax({
         url: href,
         dataType: 'html',
-        success: success
+        success: success,
+        error: removeLoading
     });
     
     function success(resp) {
-        var img = $(resp).find('.blogs-block > div[id*="UpdatePanel"] > .row:not(.margin-bottom-medium) > .col-md-12 img:first-of-type'),
-            src = $(img).attr('src');
+        var img = $(resp).find('.blogs-block .blog-featured-image-row img');
+            
+        if (!($(img).attr('src'))) {
+            img = $(resp).find('.blogs-block > div[id*="UpdatePanel"] > .row:not(.margin-bottom-medium) > .col-md-12 img:first-of-type')
+        }
+        var src = $(img).attr('src');
 
         if (!!src) {
             var url = "url('" + src + "')";
-            $(self).find('.img-container').css('background-image', url);
-            $(self).find('.img-container').removeClass('loading');
+            if (asBackground) {
+                $(self).css('background-image', url);
+            } else {
+                $(self).find('.img-container').css('background-image', url);
+            }
+        } else {
+            $(self).find('.img-container').addClass('no-ajax-image');
         }
-            
+        
+        removeLoading();
+    }
+
+    function removeLoading() {
+        $(self).find('.img-container').removeClass('loading');
     }
 }
 
