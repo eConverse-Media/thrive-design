11f5d7251208ea96e341e70a3935feb4f24f78a7
eCM-MDykeman <64661174+eCM-MDykeman@users.noreply.github.com>
2024-01-02 09:19:11 -0500

    MD | Adding make carousel tiles responsive js changes

--- code/thrive-design-general.js
+++ code/thrive-design-general.js
@@ -86,7 +86,16 @@ function handleSlider() {
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-arrow next-arrow"><i class="fa-solid fa-chevron-right"></i></button>'
+        nextArrow: '<button type="button" class="slick-arrow next-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+              }}]
     });
 
 
