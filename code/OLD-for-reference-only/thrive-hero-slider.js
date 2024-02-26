9e1b5b7042d0d1a867df65a17144cef54f7eb89d
eCM-MDykeman <64661174+eCM-MDykeman@users.noreply.github.com>
2024-02-26 14:26:43 -0500

    Updating the file structure for old files

--- code/OLD-for-reference-only/thrive-hero-slider.js
+++ code/OLD-for-reference-only/thrive-hero-slider.js
@@ -1,14 +1,4 @@
-/* function handleHeroCarousel() {
-    $('.hero.slider .card').wrapAll('<div class="hero-carousel slick-dotted card" />').removeClass('card');
-    $('.hero-carousel').slick({
-        arrows: true,
-        dots: false,
-        autoplay: false,
-        infinite: true,
-        prevArrow: '<button type="button" class="slick-arrow prev-arrow"><i class="fa-regular fa-arrow-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-arrow next-arrow"><i class="fa-regular fa-arrow-right"></i></button>'
-    });
-} */
+
 
 /*** ERS Attempts Oct 13/23 ***/
 function handleHeroCarousel() {
